b613776a0206639effcfbdafa752668f41922796
urashy <berkanakin05@gmail.com>
2025-11-10 10:59:46 +0100

    Modif berkan + edwin classes code first

---
+++ Api_c_sharp/Api_c_sharp/Models/APourCouleur.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_c_sharp.Models
+{
+    [Table("t_j_apourcouleur_apc")]
+
+    public class APourCouleur
+    {
+        [Key]
+        [Column("cou_id")]
+        public int IdCouleur { get; set; }
+
+        [Key]
+        [Column("voi_id")]
+        public int IdVoiture { get; set; }
+
+        [ForeignKey("voi_idcouleur")]
+        [InverseProperty(nameof(Voiture.Couleurs))]
+        public virtual Voiture? APourCouleurVoitureNavigation { get; set; } = null!;
+
+        [ForeignKey("voi_idcouleur")]
+        [InverseProperty(nameof(Couleur.Voitures))]
+        public virtual Couleur? APourCouleurCouleurNavigation { get; set; } = null!;
+    }
+}
--- Api_c_sharp/Api_c_sharp/obj/Debug/net8.0/Api_c_sharp.MvcApplicationPartsAssemblyInfo.cs
+++ Api_c_sharp/Api_c_sharp/obj/Debug/net8.0/Api_c_sharp.MvcApplicationPartsAssemblyInfo.cs
@@ -11,13 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Api_c_sharp")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc237006baa44fd5cdbb0e9fe4a56ec6d9a2135")]
-[assembly: System.Reflection.AssemblyProductAttribute("Api_c_sharp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Api_c_sharp")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Swashbuckle.AspNetCore.SwaggerGen")]
 
 // Généré par la classe MSBuild WriteCodeFragment.
 
