43f85cd395b232e63efb5b41c06f3edd001a2183
urashy <berkanakin05@gmail.com>
2025-11-13 08:35:21 +0100

    modif managers

---
+++ Api_c_sharp/Api_c_sharp/Models/Repository/Managers/CompteManager.cs
@@ -0,0 +1,27 @@
+﻿using Api_c_sharp.Models.Repository.Interfaces;
+using Api_c_sharp.Models.Repository.Managers;
+
+namespace Api_c_sharp.Models.Repository.Managers
+{
+    public class CompteManager : SearchableManager<Compte>, WritableRepository<Compte>
+    {
+        public CompteManager(AutoPulseBdContext context) : base(context)
+        {
+        }
+
+        public Task<Compte> AddAsync(Compte entity)
+        {
+
+        }
+
+        public Task DeleteAsync(Compte entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Compte entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
