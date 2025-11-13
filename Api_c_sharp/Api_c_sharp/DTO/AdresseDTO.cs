e0c1ead64067f9fd462667eb243e1801bf1d261a
mael <mael.trouba@etu.univ-smb.fr>
2025-11-13 09:38:24 +0100

    Début DTO

---
+++ Api_c_sharp/Api_c_sharp/DTO/AdresseDTO.cs
@@ -0,0 +1,10 @@
+namespace Api_c_sharp.DTO;
+
+public class AdresseDTO
+{
+    public int IdAdresse { get; set; }
+    public string Numero { get; set; }
+    public string Rue { get; set; }
+    public string LibelleVille { get; set; }
+    public string CodePostal { get; set; }
+}
--- Api_c_sharp/Api_c_sharp/Mapper/MapperProfile.cs
+++ Api_c_sharp/Api_c_sharp/Mapper/MapperProfile.cs
@@ -1,6 +1,16 @@
+using Api_c_sharp.DTO;
+using Api_c_sharp.Models;
+using AutoMapper;
+
 namespace Api_c_sharp.Mapper;
 
-public class MapperProfile
+public class MapperProfile: Profile
 {
-    
+    public MapperProfile()
+    {
+        CreateMap<Adresse, AdresseDTO>()
+            .ForMember(dest => dest.LibelleVille, opt => opt.MapFrom(src => src.VilleAdresseNav.Libelle))
+            .ForMember(dest => dest.CodePostal, opt => opt.MapFrom(src => src.VilleAdresseNav.CodePostal))
+            .ReverseMap();
+    }
 }
