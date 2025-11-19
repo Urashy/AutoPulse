4f0e35a4b9e8ffa7e3cae5292c0e68b193fa29fb
megiasv <megiasv@D252-S28>
2025-11-19 09:04:16 +0100

    couleur & type de compte (manager et controller)

---
+++ Api_c_sharp/Api_c_sharp/Controllers/CouleurController.cs
@@ -0,0 +1,52 @@
+﻿using Api_c_sharp.DTO;
+using Api_c_sharp.Models.Repository.Managers;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_c_sharp.Controllers
+{
+    /// <summary>
+    /// Contrôleur REST permettant de gérer les couleurs.
+    /// Les méthodes exposent ou consomment des DTO afin
+    /// d’assurer la séparation entre le modèle de domaine
+    /// et la couche API.
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CouleurController (CouleurManager _manager, IMapper _couleurMapper) : ControllerBase
+    {
+        /// <summary>
+        /// Récupère une couleur à partir de son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant unique du modele recherchée.</param>
+        /// <returns>
+        /// <item><description><see cref="CouleurDTO"/> si la couleur existe (200 OK).</description></item>
+        /// <item><description><see cref="NotFoundResult"/> si aucune couleur ne correspond (404).</description></item>
+        /// </list>
+        /// </returns>
+        [ActionName("GetById")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CouleurDTO>> GetById(int id)
+        {
+            var result = await _manager.GetByIdAsync(id);
+
+            if (result is null)
+                return NotFound();
+
+            return new ActionResult<CouleurDTO>(_couleurMapper.Map<CouleurDTO>(result));
+        }
+        /// <summary>
+        /// Récupère la liste de toutes les couleurs.
+        /// </summary>
+        /// <returns>
+        /// Une liste de <see cref="CouleurDTO"/> (200 OK).
+        /// </returns>
+        [HttpGet]
+        [ActionName("GetAll")]
+        public async Task<ActionResult<IEnumerable<CouleurDTO>>> GetAll()
+        {
+            var list = await _manager.GetAllAsync();
+            return new ActionResult<IEnumerable<CouleurDTO>>(_couleurMapper.Map<IEnumerable<CouleurDTO>>(list));
+        }
+    }
+}
---
+++ Api_c_sharp/Api_c_sharp/Controllers/TypeCompteController.cs
@@ -0,0 +1,52 @@
+﻿using Api_c_sharp.DTO;
+using Api_c_sharp.Models.Repository.Managers;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api_c_sharp.Controllers
+{
+    /// <summary>
+    /// Contrôleur REST permettant de gérer les types de compte.
+    /// Les méthodes exposent ou consomment des DTO afin
+    /// d’assurer la séparation entre le modèle de domaine
+    /// et la couche API.
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TypeCompteController (TypeCompteManager _manager, IMapper _typeCompteMapper) : ControllerBase
+    {
+        /// <summary>
+        /// Récupère untype de compte à partir de son identifiant.
+        /// </summary>
+        /// <param name="id">Identifiant unique du modele recherchée.</param>
+        /// <returns>
+        /// <item><description><see cref="TypeCompteDTO"/> si le type de compte existe (200 OK).</description></item>
+        /// <item><description><see cref="NotFoundResult"/> si aucun type de compte ne correspond (404).</description></item>
+        /// </list>
+        /// </returns>
+        [ActionName("GetById")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TypeCompteDTO>> GetById(int id)
+        {
+            var result = await _manager.GetByIdAsync(id);
+
+            if (result is null)
+                return NotFound();
+
+            return new ActionResult<TypeCompteDTO>(_typeCompteMapper.Map<TypeCompteDTO>(result));
+        }
+        /// <summary>
+        /// Récupère la liste de toutes les types de compte.
+        /// </summary>
+        /// <returns>
+        /// Une liste de <see cref="TypeCompteDTO"/> (200 OK).
+        /// </returns>
+        [HttpGet]
+        [ActionName("GetAll")]
+        public async Task<ActionResult<IEnumerable<TypeCompteDTO>>> GetAll()
+        {
+            var list = await _manager.GetAllAsync();
+            return new ActionResult<IEnumerable<TypeCompteDTO>>(_typeCompteMapper.Map<IEnumerable<TypeCompteDTO>>(list));
+        }
+    }
+}
