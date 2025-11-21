ecac27e8b229da45f1fab51991a1778435f302c0
mathieu <mathieu.servonnet@etu.univ-smb.fr>
2025-11-21 09:18:32 +0100

    modif adresse tout nul plus manager plus dto plus mapper

--- Api_c_sharp/Api_c_sharp/Models/Repository/Managers/WritableManager.cs
+++ Api_c_sharp/Api_c_sharp/Models/Repository/Managers/WritableManager.cs
@@ -6,30 +6,32 @@ namespace Api_c_sharp.Models.Repository.Managers
     public abstract class WritableManager<TEntity> :  WritableRepository<TEntity>
     where TEntity : class
     {
-        protected readonly AutoPulseBdContext context;
+        protected readonly AutoPulseBdContext _context;
         protected readonly DbSet<TEntity> dbSet;
 
         protected WritableManager(AutoPulseBdContext context)
         {
+            _context = context;
+            dbSet = context.Set<TEntity>();
         }
 
         public virtual async Task<TEntity> AddAsync(TEntity entity)
         {
             await dbSet.AddAsync(entity);
-            await context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
             return entity;
         }
 
         public virtual async Task UpdateAsync(TEntity entityToUpdate, TEntity entity)
         {
-            context.Entry(entityToUpdate).CurrentValues.SetValues(entity);
-            await context.SaveChangesAsync();
+            _context.Entry(entityToUpdate).CurrentValues.SetValues(entity);
+            await _context.SaveChangesAsync();
         }
 
         public virtual async Task DeleteAsync(TEntity entity)
         {
             dbSet.Remove(entity);
-            await context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
         }
     }
 }
