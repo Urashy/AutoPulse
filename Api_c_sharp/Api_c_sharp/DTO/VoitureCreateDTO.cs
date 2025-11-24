f2e24f9b064cda01ac4cc7d74e8f792145616f93
mathieu <mathieu.servonnet@etu.univ-smb.fr>
2025-11-24 10:30:06 +0100

    AJout de 4 TEST et modif de DTO

--- Api_c_sharp/Api_c_sharp/DTO/VoitureCreateDTO.cs
+++ Api_c_sharp/Api_c_sharp/DTO/VoitureCreateDTO.cs
@@ -8,7 +8,6 @@ public class VoitureCreateDTO
     public int IdMotricite { get; set; }
     public int IdCarburant { get; set; }
     public int IdBoiteDeVitesse { get; set; }
-    public int IdCouleur { get; set; }
     public int IdCategorie { get; set; }
     public int Kilometrage { get; set; }
     public int Annee { get; set; }
@@ -17,4 +16,6 @@ public class VoitureCreateDTO
     public int NbCylindres { get; set; }
     public DateTime MiseEnCirculation { get; set; }
     public int? IdModeleBlender { get; set; }
+    public int NbPlace { get; set; }
+    public int NbPorte { get; set; }
 }
