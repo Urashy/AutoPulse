c28e1c6e7d78a23eff35b7349c3cafa97e4dcf9b
mathieu <mathieu.servonnet@etu.univ-smb.fr>
2025-11-28 09:16:07 +0100

    Modif order by

--- Api_c_sharp/Api_c_sharp/Models/Repository/Managers/Models Manager/TypeSignalement.cs
+++ Api_c_sharp/Api_c_sharp/Models/Repository/Managers/Models Manager/TypeSignalement.cs
@@ -1,9 +1,18 @@
-﻿namespace Api_c_sharp.Models.Repository.Managers
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Api_c_sharp.Models.Repository.Managers
 {
     public class TypeSignalementManager : ReadableManager<TypeSignalement>
     {
         public TypeSignalementManager(AutoPulseBdContext context) : base(context)
         {
+        }
+
+        public override async Task<IEnumerable<TypeSignalement>> GetAllAsync()
+        {
+            return await dbSet
+                .OrderBy(m => m.LibelleTypeSignalement)
+                .ToListAsync();
         }
     }
 }
