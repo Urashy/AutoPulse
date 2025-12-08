ff4beae5653c290339d463c2135349eaa7c46ae2
mael <mael.trouba@etu.univ-smb.fr>
2025-12-08 12:12:28 +0100

    Masquer annonce et démasquer

--- Api_c_sharp/BlazorAutoPulse/Service/Interface/IAnnonceService.cs
+++ Api_c_sharp/BlazorAutoPulse/Service/Interface/IAnnonceService.cs
@@ -1,18 +1,19 @@
 ﻿using AutoPulse.Shared.DTO;
 using BlazorAutoPulse.Model;
+using AnnonceDetailDTO = AutoPulse.Shared.DTO.AnnonceDetailDTO;
 
 namespace BlazorAutoPulse.Service.Interface
 {
-    public interface IAnnonceService : IService<Annonce>
+    public interface IAnnonceService : IService<AnnonceDTO>
     {
         Task<IEnumerable<AnnonceDTO>> GetByIdMiseEnAvant(int id, int pageNumber = 1, int pageSize = 21);
+        Task<AnnonceDTO> CreateAnnonceAsync(AnnonceCreateUpdateDTO entity);
+        Task UpdateAnnonceAsync(int id, AnnonceCreateUpdateDTO entity);
         Task<IEnumerable<AnnonceDTO>> GetFilteredAnnoncesAsync(ParametreRecherche searchParams);
         Task<IEnumerable<AnnonceDTO>> GetByCompteID(int id);
         Task<IEnumerable<AnnonceDTO>> GetAnnoncesFavoritesByCompteId(int compteId);
-
-    }
-
-    public interface IAnnonceDetailService : IService<BlazorAutoPulse.Model.AnnonceDetailDTO>
-    {
+        Task<bool> EstMasquerAsync(int id);
+        
+        Task<AnnonceDetailDTO> GetAnnonceDetailById(int id);
     }
 }
