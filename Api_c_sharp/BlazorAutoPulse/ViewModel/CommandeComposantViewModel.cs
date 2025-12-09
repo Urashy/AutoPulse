0a07ace30a8b2a65b6cbde7fed7aeea3b4cb0749
TOOGB <yahia.el-ghouzli@etu.univ-smb.fr>
2025-12-09 17:48:02 +0100

    Ajout commande composant

---
+++ Api_c_sharp/BlazorAutoPulse/ViewModel/CommandeComposantViewModel.cs
@@ -0,0 +1,36 @@
+﻿using AutoPulse.Shared.DTO;
+using BlazorAutoPulse.Service.Interface;
+
+namespace BlazorAutoPulse.ViewModel
+{
+    public class CommandeComposantViewModel
+    {
+        private readonly ICompteService _compteService;
+
+        public bool IsFavorite { get; private set; }
+        private int? _currentUserId;
+        private Action? _refreshUI;
+
+        public CommandeComposantViewModel(
+            ICompteService compteService)
+        {
+            _compteService = compteService;
+        }
+
+        public async Task InitializeAsync(CommandeDTO commande, Action refreshUI)
+        {
+            _refreshUI = refreshUI;
+
+            try
+            {
+                var compte = await _compteService.GetMe();
+                _currentUserId = compte?.IdCompte;
+            }
+            catch
+            {
+                _currentUserId = null;
+                IsFavorite = false;
+            }
+        }
+    }
+}
