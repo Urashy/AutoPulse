25354984fcaddd8dca0ebf8c38520895a8869660
Urashy <berkanakin05@gmail.com>
2025-12-09 15:20:27 +0100

    dto mathieu

---
+++ Api_c_sharp/AutoPulse.Shared/DTO/PlainteCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoPulse.Shared.DTO
+{
+    public class PlainteCreateDTO
+    {
+        [Required(ErrorMessage = "Le contenu de la plainte est obligatoire.")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Le contenu doit faire entre 10 et 1000 caractères.")]
+        public string Contenu { get; set; }
+    }
+}
---
+++ Api_c_sharp/BlazorAutoPulse/Service/Interface/IPlainteService.cs
@@ -0,0 +1,10 @@
+﻿using AutoPulse.Shared.DTO;
+
+namespace BlazorAutoPulse.Service.Interface
+{
+    // Assumons que ServiceResult est utilisé pour gérer la réponse
+    public interface IPlainteService
+    {
+        Task<ServiceResult> EnvoyerPlainteAsync(PlainteCreateDTO plainteDTO);
+    }
+}
