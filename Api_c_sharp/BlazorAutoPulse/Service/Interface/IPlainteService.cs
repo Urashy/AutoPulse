f8701863e37c0bf1385a8c5785a7040897c9c6d7
Urashy <berkanakin05@gmail.com>
2025-12-09 15:26:43 +0100

    ajout plainte front

--- Api_c_sharp/BlazorAutoPulse/Service/Interface/IPlainteService.cs
+++ Api_c_sharp/BlazorAutoPulse/Service/Interface/IPlainteService.cs
@@ -2,9 +2,9 @@
 
 namespace BlazorAutoPulse.Service.Interface
 {
-    // Assumons que ServiceResult est utilisé pour gérer la réponse
     public interface IPlainteService
     {
-        Task<ServiceResult> EnvoyerPlainteAsync(PlainteCreateDTO plainteDTO);
+        Task<PlainteDTO> CreatePlainteAsync(PlainteCreateDTO entity);
+
     }
 }
---
+++ Api_c_sharp/BlazorAutoPulse/Service/WebService/PlainteWebService.cs
@@ -0,0 +1,35 @@
+﻿using AutoPulse.Shared.DTO;
+using BlazorAutoPulse.Service.Interface;
+using System.Net.Http.Json;
+
+namespace BlazorAutoPulse.Service.WebService
+{
+    // Hérédité de BaseWebService (présumé)
+    public class PlainteWebService : BaseWebService<PlainteCreateDTO>, IPlainteService
+    {
+        public PlainteWebService(HttpClient httpClient) : base(httpClient) { }
+
+        protected override string ApiEndpoint => "Plainte";
+
+        public async Task<PlainteDTO> CreatePlainteAsync(PlainteCreateDTO plainteDTO)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, BuildUrl("Post"))
+            {
+                Content = JsonContent.Create(plainteDTO)
+            };
+
+            var response = await SendWithCredentialsAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return await response.Content.ReadFromJsonAsync<PlainteDTO>();
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Erreur Post : {error}");
+                return await response.Content.ReadFromJsonAsync<PlainteDTO>();
+            }
+        }
+    }
+}
