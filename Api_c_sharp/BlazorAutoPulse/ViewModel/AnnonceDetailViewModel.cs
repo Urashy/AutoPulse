76b4ce08c58c6e1a18395eecd7b866b506f98c00
Urashy <berkanakin05@gmail.com>
2025-12-09 10:02:02 +0100

    annonce détail

--- Api_c_sharp/BlazorAutoPulse/ViewModel/AnnonceDetailViewModel.cs
+++ Api_c_sharp/BlazorAutoPulse/ViewModel/AnnonceDetailViewModel.cs
@@ -359,7 +359,7 @@ namespace BlazorAutoPulse.ViewModel
                 Prix = Annonce.Prix,
                 Description = Annonce.Libelle
             };
-            _annonceService.UpdateAnnonceAsync(Annonce.IdAnnonce, annonceChange);
+            await _annonceService.UpdateAnnonceAsync(Annonce.IdAnnonce, annonceChange);
             IsOptionsMenuOpen = false;
 
             await EstMasquerAnnonce();
