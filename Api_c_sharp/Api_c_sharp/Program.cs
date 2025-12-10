55116e910ac8e4b81e580782f41f7e3b7dab9100
akopiane <edwin.akopian@etu.univ-smb.fr>
2025-12-10 09:27:14 +0100

    Modif connection BD

--- Api_c_sharp/Api_c_sharp/Program.cs
+++ Api_c_sharp/Api_c_sharp/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //------------------------------Connection DB------------------------------
-var connectionString = builder.Configuration.GetConnectionString("LocaleConnection");
+var connectionString = builder.Configuration.GetConnectionString("AzureConnection");
 
 builder.Services.AddDbContext<AutoPulseBdContext>(options =>
     options.UseNpgsql(connectionString));
