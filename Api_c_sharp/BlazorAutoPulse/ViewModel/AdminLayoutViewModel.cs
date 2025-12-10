32c2f0d214fabfaaf4ee18122e5da831254cbee9
Urashy <berkanakin05@gmail.com>
2025-12-10 21:58:09 +0100

    vérif propre avec redirection AVANT chargement de la page admin

--- Api_c_sharp/BlazorAutoPulse/ViewModel/AdminLayoutViewModel.cs
+++ Api_c_sharp/BlazorAutoPulse/ViewModel/AdminLayoutViewModel.cs
@@ -1,45 +1,55 @@
-﻿using AutoPulse.Shared.DTO;
-using BlazorAutoPulse.Service.Interface;
+﻿using BlazorAutoPulse.Service.Interface;
 using Microsoft.AspNetCore.Components;
 
-namespace BlazorAutoPulse.ViewModel
+public class AdminLayoutViewModel
 {
-    public class AdminLayoutViewModel
+    private readonly ICompteService _compteService;
+    private readonly NavigationManager _nav;
+
+    private Action? _refreshUI;
+
+    public bool IsVerified { get; private set; } = false;
+    public bool IsLoading { get; private set; } = true;
+
+    public AdminLayoutViewModel(ICompteService compteService, NavigationManager nav)
     {
-        private readonly ICompteService _compteService;
-        private readonly NavigationManager _nav; 
+        _compteService = compteService;
+        _nav = nav;
+    }
 
-        private Action? _refreshUI;
-        public AdminLayoutViewModel(ICompteService compteService, NavigationManager nav)
+    public async Task InitializeAsync(Action refreshUI)
+    {
+        _refreshUI = refreshUI;
+        await VerifUserAdmin();
+    }
+
+    private async Task VerifUserAdmin()
+    {
+        IsLoading = true;
+        _refreshUI?.Invoke();
+
+        try
         {
-            _compteService = compteService;
-            _nav = nav;
-        }
+            var compte = await _compteService.GetMe();
 
-        public async Task InitializeAsync(Action refreshUI)
-        {
-            _refreshUI = refreshUI;
-
-            verifUserAdmin();
-        }
-
-        private async void verifUserAdmin()
-        {
-            CompteDetailDTO compte;
-
-            try
+            if (compte.IdTypeCompte != 3)
             {
-                compte = await _compteService.GetMe();
+                _nav.NavigateTo("", forceLoad: true);
+                return;
+            }
 
-                if (compte.IdTypeCompte != 3)
-                {
-                    _nav.NavigateTo(""); 
-                }
-            }
-            catch
-            {
-                _nav.NavigateTo("connexion");
-            }
+            IsVerified = true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erreur vérification admin: {ex.Message}");
+            _nav.NavigateTo("connexion", forceLoad: true);
+            return;
+        }
+        finally
+        {
+            IsLoading = false;
+            _refreshUI?.Invoke();
         }
     }
 }
