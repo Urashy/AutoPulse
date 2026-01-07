dc20c7649b4d1102fdc468a9eb6b1bb264d4582b
mathieu <mathieu.servonnet@etu.univ-smb.fr>
2026-01-07 09:07:24 +0100

    Nouvelle table et migration

---
+++ Api_c_sharp/Api_c_sharp/Models/Entity/EtatCommande.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_c_sharp.Models.Entity
+{
+    [Table("t_e_etatcommande_etc")]
+    public class EtatCommande
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("etc_id")]
+        public int IdEtatCommande { get; set; }
+
+        [Required]
+        [Column("etc_libelle")]
+        public string Libelle { get; set; } = null!;
+
+        [InverseProperty(nameof(Commande.CommandeEtatCommandeNav))]
+        public virtual ICollection<Commande> Commandes { get; set; } = new List<Commande>();
+    }
+}
