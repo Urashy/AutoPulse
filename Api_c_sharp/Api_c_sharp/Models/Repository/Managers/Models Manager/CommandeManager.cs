ab082b852664a7454e2d5b7166b7afd54055b9b0
Urashy <berkanakin05@gmail.com>
2026-01-14 08:40:40 +0100

    suppression using blazor dans commande manager

--- Api_c_sharp/Api_c_sharp/Models/Repository/Managers/Models Manager/CommandeManager.cs
+++ Api_c_sharp/Api_c_sharp/Models/Repository/Managers/Models Manager/CommandeManager.cs
@@ -1,6 +1,5 @@
 ﻿using Api_c_sharp.Models.Entity;
 using Api_c_sharp.Models.Repository.Interfaces;
-using BlazorAutoPulse.Service;
 using Microsoft.EntityFrameworkCore;
 
 namespace Api_c_sharp.Models.Repository.Managers.Models_Manager
