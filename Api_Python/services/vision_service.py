69c8b8b00923d44f4ad3e1456368ac2f74136dc9
Urashy <berkanakin05@gmail.com>
2026-01-17 05:00:33 +0100

    tezt push

--- Api_Python/services/vision_service.py
+++ Api_Python/services/vision_service.py
@@ -12,7 +12,7 @@ from schemas.dto import DataCNN, ResultatCNN, TopPrediction
 from repositories.repository_manager import ModelRepositoryManager
 
 logger = logging.getLogger(__name__)
-
+#test
 
 class VisionRecognitionService(IModelService):
     """Service pour identifier un véhicule depuis une image"""
@@ -27,7 +27,6 @@ class VisionRecognitionService(IModelService):
     def predict(self, data: DataCNN) -> ResultatCNN:
         """
         Identifie un véhicule depuis une image encodée en base64
-        
         Args:
             data: Image encodée en base64
             
