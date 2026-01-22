09421b69edb756d8767b700a76c9b4bfbfa659a5
BerkanAkin001 <155959102+BerkanAkin001@users.noreply.github.com>
2026-01-22 20:44:19 +0100

    correction gmailpass dans tockenemailcontroller

--- Api_c_sharp/Api_c_sharp/Controllers/TokenEmailController.cs
+++ Api_c_sharp/Api_c_sharp/Controllers/TokenEmailController.cs
@@ -139,8 +139,8 @@ public class TokenEmailController(
             Text = corpsMessage
         };
 
-        string? user = _config["Email:GmailUser"];
-        string? password = _config["Email:GmailPass"];
+        string? user = _config["GmailUser"] ?? _config["Email:GmailUser"];
+        string? password = _config["GmailPass"] ?? _config["Email:GmailPass"];
 
         using var client = new SmtpClient();
         await client.ConnectAsync("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
